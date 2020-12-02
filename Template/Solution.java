3b6883f33a441f25d5448d5c21d1110b10e32a74
TealStone <ultratealstone@gmail.com>
2020-12-02 16:31:28 -0500

    Made a template for faster solving

---
+++ Template/Solution.java
@@ -0,0 +1,15 @@
+package Day;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("Day2/input.txt"));
+
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+        }
+    }
+}
